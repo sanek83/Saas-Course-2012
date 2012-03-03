7d100dba069907b4d79fdc81696d0ec7dcf3b48b
shureg <4shureg@gmail.com>
2012-03-03 22:09:23 +0100

    part 2

--- homework1/part2.rb
+++ homework1/part2.rb
@@ -1,19 +1,25 @@
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
-  
+
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
   raise NoSuchStrategyError unless ['p','r','s'].include?(game[0][1].downcase) and ['p','r','s'].include?(game[1][1].downcase)
-  
-  if ((game[0][1].downcase == game[1][1].downcase) or (game[0][1].downcase == 'r' and game[1][1].downcase == 's') or (game[0][1].downcase == 's' and game[1][1].downcase == 'p')or (game[0][1].downcase == 'p' and game[1][1].downcase == 'r')) then
-    'returns the list ' + game[0].to_s + ' wins since '+ game[0][1]+'>'+game[1][1]
+  play = [game[0][1].downcase, game[1][1].downcase]
+  return game[0] if play[0] == play[1] 
+
+  if play == ["r","s"] or play == ["p","r"] or play == ["s","p"] then
+    return game[0]
   else
-    'returns the list ' + game[1].to_s + " wins since "+ game[1][1]+">"+game[0][1]
-  end     
+    return game[1]
+  end
 end
 
-p rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ])
-p rps_game_winner([ [ "Armando", "S" ], [ "Dave", "S" ] ])
-p rps_game_winner([ [ "Armando", "r" ], [ "Dave", "S" ] ])
-p rps_game_winner([ [ "Armando", "P" ], [ "Dave", "r" ] ])
+def rps_tournament_winner(game)
+  if game[0][0].is_a?(String)
+    return rps_game_winner(game)
+  else
+    rps_tournament_winner([rps_tournament_winner(game[0]), rps_tournament_winner(game[1])])
+  end
+end
+
 
